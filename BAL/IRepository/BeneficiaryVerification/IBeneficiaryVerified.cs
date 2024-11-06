57f92a37be59b850b4c564a1355a466ed597b73d
Sulaman1 <khansulaman303@gmail.com>
2024-11-06 11:50:28 +0500

    benef10

---
+++ BAL/IRepository/BeneficiaryVerification/IBeneficiaryVerified.cs
@@ -0,0 +1,31 @@
+﻿using DAL.Models.Domain.MasterSetup;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL.IRepository.BeneficiaryVerification
+{
+    public interface IBeneficiaryVerified
+    {
+        Task<List<BeneficiaryVerified>> GetAll();
+
+        Task<BeneficiaryVerified> GetById(int? Id);
+
+        Task Insert(BeneficiaryVerified member, IFormFile ProfilePicture);
+
+        Task Update(BeneficiaryVerified member, IFormFile ProfilePicture);
+
+        void Remove(BeneficiaryVerified member);
+
+        Task Save();        
+
+        int Count(string cnic);
+
+        int CountCell(string cell);
+
+        int Max();
+    }
+}
